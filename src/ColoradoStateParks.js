56b490f46d8a957132d67c1fdcb1c2db311476ff
jay <jaypifiverr@gmail.com>
2022-09-08 22:57:03 -0400

    commit tree c84efeb2b80d1dc28c46007c20c797e2a92b0e00

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,30 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife } from "./parks/RockyMountain";
+// import {trees as parkTrees, wildlife as parkWildlife,} from "./parks/RockyMountain";
+
+import * as RMFunctions from "./parks/RockyMountain"
+
+console.log(RMFunctions.trees);
+
+RMFunctions.wildlife();
+
+RMFunctions.elevation();
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+  <div>
+    <h1>Colorado State Parks!</h1>
+    <MesaVerde/>
+  </div>
+  );
 }
+
+console.log(trees);
+
+wildlife();
+
+export default ColoradoStateParks;
